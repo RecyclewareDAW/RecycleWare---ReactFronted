25379c83bc49d3f4591eb93db2af0143fda350f4
Bela <belasimonyan@gmail.com>
2026-03-02 12:31:03 +0100

    Corrigiendo imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './scss/main.scss'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import App from './App.jsx'
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 //Home
 import { Link } from 'react-router-dom';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
 export default function Home() {
     return <>
-        <h1 className='display-1'>Patata Placeholder<p className="bi bi-briefcase text-danger"></p></h1>
+        {/* <h1 className='display-1'>Patata Placeholder<p className="bi bi-briefcase text-danger"></p></h1>
         <div className="bi bi-star-fill text-danger">Estrella</div>
-        <Link to="/estrella" className='btn btn-sm text-danger'>Navegación de testeo</Link>
+        <Link to="/estrella" className='btn btn-sm text-danger'>Navegación de testeo</Link> */}
+        <Header />
+        <Hero />
         
     </>
 }
