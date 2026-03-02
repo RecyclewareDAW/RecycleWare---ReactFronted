4535593975029345e34ea9ced8ca1bc610f81c7f
Bela <belasimonyan@gmail.com>
2026-03-02 12:39:39 +0100

    Header y Hero listos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,29 @@ export default function Home() {
         {/* <h1 className='display-1'>Patata Placeholder<p className="bi bi-briefcase text-danger"></p></h1>
         <div className="bi bi-star-fill text-danger">Estrella</div>
         <Link to="/estrella" className='btn btn-sm text-danger'>Navegación de testeo</Link> */}
-        <Header />
-        <Hero />
-        
+        <div
+            data-bs-spy="scroll"
+            data-bs-target=".navbar"
+            data-bs-smooth-scroll="true"
+            tabIndex={0}
+        >
+            <div className="d-flex flex-column min-vh-100">
+                <Header />
+                <Hero />
+            </div>
+{/* 
+            <main>
+                <Categorias />
+                <hr />
+                <Comunidad />
+                <hr className="my-5 opacity-25" />
+                <Info />
+                <hr />
+                <Contacto />
+            </main>
+
+            <Footer /> */}
+        </div>
+
     </>
 }
