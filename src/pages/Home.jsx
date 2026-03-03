88d8001cb50efd44f14b6d878cd6fbe960e04fd5
Bela <belasimonyan@gmail.com>
2026-03-03 13:33:48 +0100

    Arreglado enlaces de nav

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useLocation, Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Hero from '../components/Home/Hero';
 import Info from '../components/Home/Info';
@@ -8,6 +9,25 @@ import Footer from '../components/Footer';
 import Comunidad from '../components/Home/Comunidad';
 
 export default function Home() {
+    const location = useLocation();
+    useEffect(() => {
+        if (location.hash) {
+            // Buscamos el elemento (quitando el '#' de la cadena)
+            const id = location.hash.replace('#', '');
+            const element = document.getElementById(id);
+            
+            if (element) {
+                // Hacemos scroll suave. El setTimeout es necesario a veces 
+                // para darle a React unos milisegundos para renderizar la página antes de moverla
+                setTimeout(() => {
+                    element.scrollIntoView({ behavior: 'smooth' });
+                }, 100); 
+            }
+        } else {
+            // Si la URL no tiene '#', nos aseguramos de estar arriba del todo
+            window.scrollTo(0, 0);
+        }
+    }, [location]); // Se ejecuta cada vez que cambia la URL
     return <>
         {/* <h1 className='display-1'>Patata Placeholder<p className="bi bi-briefcase text-danger"></p></h1>
         <div className="bi bi-star-fill text-danger">Estrella</div>
