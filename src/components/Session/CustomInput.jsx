78267f4c73c2f74af31926ed32d1658ed4fcef21
RicardoIESDB <rl.aucapina@iesdoctorbalmis.com>
2026-03-04 13:55:32 +0100

    Se modifica CustomInput

--- src/components/Session/CustomInput.jsx
+++ src/components/Session/CustomInput.jsx
@@ -13,7 +13,28 @@ const FormInput = ({ id, label, type, placeholder, required, errorMessage, hideL
         <label className="form-check-label text-muted" htmlFor={id}>
           {label}
         </label>
-        {/* Aquí mostramos el mensaje de error de Bootstrap */}
+        <div className="invalid-feedback">
+          {errorMessage}
+        </div>
+      </div>
+    );
+  }
+
+  // Si es un Textarea
+  if (type === "textarea") {
+    return (
+      <div className="w-100">
+        <label htmlFor={id} className={`form-label ${hideLabel ? 'd-none' : ''}`}>
+          {label}
+        </label>
+        <textarea
+          className="form-control rounded-0 px-0 shadow-none text-primary inputs"
+          id={id}
+          name={id}
+          rows="2"
+          placeholder={placeholder}
+          required={required}
+        ></textarea>
         <div className="invalid-feedback">
           {errorMessage}
         </div>
@@ -22,16 +43,19 @@ const FormInput = ({ id, label, type, placeholder, required, errorMessage, hideL
   }
 
   return (
-    <div>
-      <label htmlFor={id}>{label}</label>
+    <div className="w-100">
+      <label htmlFor={id} className={`form-label ${hideLabel ? 'd-none' : ''}`}>{label}</label>
       <input
-        className="inputs"
+        className="form-control rounded-0 px-0 shadow-none text-primary inputs"
         id={id}
         type={type}
         name={id}
         placeholder={placeholder}
         required={required}
       />
+      <div className="invalid-feedback">
+        {errorMessage}
+      </div>
     </div>
   )
 }
