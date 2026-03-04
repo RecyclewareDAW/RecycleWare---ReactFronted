643118d8f8d0708d0c4cad655e945ac4e549f642
RicardoIESDB <rl.aucapina@iesdoctorbalmis.com>
2026-03-04 13:39:32 +0100

    Modificando CustomInput

--- src/components/Session/CustomInput.jsx
+++ src/components/Session/CustomInput.jsx
@@ -1,8 +1,31 @@
-const FormInput = ({ label, type, id, placeholder, required }) => {
+const FormInput = ({ id, label, type, placeholder, required, errorMessage, hideLabel }) => {
+  // Si el input es un checkbox 
+  if (type === "checkbox") {
+    return (
+      <div className="form-check mb-5">
+        <input
+          className="form-check-input terminos"
+          type="checkbox"
+          id={id}
+          name={id}
+          required={required}
+        />
+        <label className="form-check-label text-muted" htmlFor={id}>
+          {label}
+        </label>
+        {/* Aquí mostramos el mensaje de error de Bootstrap */}
+        <div className="invalid-feedback">
+          {errorMessage}
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div>
       <label htmlFor={id}>{label}</label>
       <input
+        className="inputs"
         id={id}
         type={type}
         name={id}
