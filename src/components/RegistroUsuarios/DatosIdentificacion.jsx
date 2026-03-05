f3d6ef1329eb83d2ab533ab354bfcf0a35fb0646
Ricardo Aucapiña <rl.aucapina@iesdoctorbalmis.com>
2026-03-06 00:32:25 +0100

    Se reemplaza codigo repetido para utilizar los componentes recientemente creados (FormCard, CustomForm, CustomInput)

--- src/components/RegistroUsuarios/DatosIdentificacion.jsx
+++ src/components/RegistroUsuarios/DatosIdentificacion.jsx
@@ -1,76 +1,141 @@
+import CustomInput from '../CustomInput';
+
 export default function DatosIdentificacion({ tipoPerfil }) {
     return (
         <>
-            <div className="mb-4">
-                <label htmlFor="nombre" className="form-label d-none">
-                    {tipoPerfil === 'particular' ? 'Nombre completo' : 'Nombre de la empresa'}
-                </label>
-                <input
-                    type="text"
-                    className="form-control rounded-0 px-0 shadow-none text-primary inputs"
-                    id="nombre"
-                    placeholder={tipoPerfil === 'particular' ? 'Nombre completo' : 'Nombre de la empresa'}
-                    required
-                />
-                <div className="invalid-feedback">Este campo es obligatorio.</div>
-            </div>
+            <CustomInput
+                id="nombre"
+                label={tipoPerfil === 'particular' ? 'Nombre completo' : 'Nombre de la empresa'}
+                type="text"
+                placeholder={tipoPerfil === 'particular' ? 'Nombre completo' : 'Nombre de la empresa'}
+                required={true}
+                hideLabel={true}
+                errorMessage="Este campo es obligatorio."
+            />
 
             {tipoPerfil === 'empresa' && (
-                <div className="mb-4">
-                    <label htmlFor="personaContacto" className="form-label d-none">Persona de contacto</label>
-                    <input
-                        type="text"
-                        className="form-control rounded-0 px-0 shadow-none text-primary inputs"
-                        id="personaContacto"
-                        placeholder="Nombre de la persona de contacto"
-                        required
-                    />
-                    <div className="invalid-feedback">Indica una persona de contacto.</div>
-                </div>
+                <CustomInput
+                    id="personaContacto"
+                    label="Persona de contacto"
+                    type="text"
+                    placeholder="Nombre de la persona de contacto"
+                    required={true}
+                    hideLabel={true}
+                    errorMessage="Indica una persona de contacto."
+                />
             )}
 
-            <div className="row g-3 mb-4">
+            <div className="row g-3">
                 <div className="col-md-6">
-                    <label htmlFor="documento" className="form-label d-none">
-                        {tipoPerfil === 'particular' ? 'DNI / NIE' : 'CIF'}
-                    </label>
-                    <input
-                        type="text"
-                        className="form-control rounded-0 px-0 shadow-none text-primary inputs"
+                    <CustomInput
                         id="documento"
+                        label={tipoPerfil === 'particular' ? 'DNI / NIE' : 'CIF'}
+                        type="text"
                         placeholder={tipoPerfil === 'particular' ? 'DNI / NIE' : 'CIF'}
-                        required
+                        required={true}
+                        hideLabel={true}
+                        errorMessage={`Por favor, introduce el ${tipoPerfil === 'particular' ? 'DNI' : 'CIF'}.`}
                     />
-                    <div className="invalid-feedback">
-                        Por favor, introduce el {tipoPerfil === 'particular' ? 'DNI' : 'CIF'}.
-                    </div>
                 </div>
                 <div className="col-md-6">
-                    <label htmlFor="telefono" className="form-label d-none">
-                        {tipoPerfil === 'particular' ? 'Teléfono' : 'Teléfono principal'}
-                    </label>
-                    <input
-                        type="tel"
-                        className="form-control rounded-0 px-0 shadow-none text-primary inputs"
+                    <CustomInput
                         id="telefono"
+                        label={tipoPerfil === 'particular' ? 'Teléfono' : 'Teléfono principal'}
+                        type="tel"
                         placeholder={tipoPerfil === 'particular' ? 'Teléfono' : 'Teléfono principal'}
-                        required
+                        required={true}
+                        hideLabel={true}
+                        errorMessage="Añade un número de contacto válido."
                     />
-                    <div className="invalid-feedback">Añade un número de contacto válido.</div>
                 </div>
             </div>
 
             {tipoPerfil === 'empresa' && (
-                <div className="mb-4">
-                    <label htmlFor="telefonoSecundario" className="form-label d-none">Teléfono secundario</label>
-                    <input
-                        type="tel"
-                        className="form-control rounded-0 px-0 shadow-none text-primary inputs"
-                        id="telefonoSecundario"
-                        placeholder="Teléfono secundario (Opcional)"
-                    />
-                </div>
+                <CustomInput
+                    id="telefonoSecundario"
+                    label="Teléfono secundario"
+                    type="tel"
+                    placeholder="Teléfono secundario (Opcional)"
+                    hideLabel={true}
+                />
             )}
         </>
     );
 }
+
+// export default function DatosIdentificacion({ tipoPerfil }) {
+//     return (
+//         <>
+//             <div className="mb-4">
+//                 <label htmlFor="nombre" className="form-label d-none">
+//                     {tipoPerfil === 'particular' ? 'Nombre completo' : 'Nombre de la empresa'}
+//                 </label>
+//                 <input
+//                     type="text"
+//                     className="form-control rounded-0 px-0 shadow-none text-primary inputs"
+//                     id="nombre"
+//                     placeholder={tipoPerfil === 'particular' ? 'Nombre completo' : 'Nombre de la empresa'}
+//                     required
+//                 />
+//                 <div className="invalid-feedback">Este campo es obligatorio.</div>
+//             </div>
+
+//             {tipoPerfil === 'empresa' && (
+//                 <div className="mb-4">
+//                     <label htmlFor="personaContacto" className="form-label d-none">Persona de contacto</label>
+//                     <input
+//                         type="text"
+//                         className="form-control rounded-0 px-0 shadow-none text-primary inputs"
+//                         id="personaContacto"
+//                         placeholder="Nombre de la persona de contacto"
+//                         required
+//                     />
+//                     <div className="invalid-feedback">Indica una persona de contacto.</div>
+//                 </div>
+//             )}
+
+//             <div className="row g-3 mb-4">
+//                 <div className="col-md-6">
+//                     <label htmlFor="documento" className="form-label d-none">
+//                         {tipoPerfil === 'particular' ? 'DNI / NIE' : 'CIF'}
+//                     </label>
+//                     <input
+//                         type="text"
+//                         className="form-control rounded-0 px-0 shadow-none text-primary inputs"
+//                         id="documento"
+//                         placeholder={tipoPerfil === 'particular' ? 'DNI / NIE' : 'CIF'}
+//                         required
+//                     />
+//                     <div className="invalid-feedback">
+//                         Por favor, introduce el {tipoPerfil === 'particular' ? 'DNI' : 'CIF'}.
+//                     </div>
+//                 </div>
+//                 <div className="col-md-6">
+//                     <label htmlFor="telefono" className="form-label d-none">
+//                         {tipoPerfil === 'particular' ? 'Teléfono' : 'Teléfono principal'}
+//                     </label>
+//                     <input
+//                         type="tel"
+//                         className="form-control rounded-0 px-0 shadow-none text-primary inputs"
+//                         id="telefono"
+//                         placeholder={tipoPerfil === 'particular' ? 'Teléfono' : 'Teléfono principal'}
+//                         required
+//                     />
+//                     <div className="invalid-feedback">Añade un número de contacto válido.</div>
+//                 </div>
+//             </div>
+
+//             {tipoPerfil === 'empresa' && (
+//                 <div className="mb-4">
+//                     <label htmlFor="telefonoSecundario" className="form-label d-none">Teléfono secundario</label>
+//                     <input
+//                         type="tel"
+//                         className="form-control rounded-0 px-0 shadow-none text-primary inputs"
+//                         id="telefonoSecundario"
+//                         placeholder="Teléfono secundario (Opcional)"
+//                     />
+//                 </div>
+//             )}
+//         </>
+//     );
+// }
--- src/components/RegistroUsuarios/DatosUbicacion.jsx
+++ src/components/RegistroUsuarios/DatosUbicacion.jsx
@@ -1,46 +1,94 @@
+import CustomInput from '../CustomInput';
+
 export default function DatosUbicacion({ tipoPerfil }) {
     return (
         <>
             {tipoPerfil === 'empresa' && (
-                <div className="mb-4">
-                    <label htmlFor="direccion" className="form-label d-none">Dirección</label>
-                    <input
-                        type="text"
-                        className="form-control rounded-0 px-0 shadow-none text-primary inputs"
-                        id="direccion"
-                        placeholder="Dirección (Calle, número, nave...)"
-                        required
-                    />
-                    <div className="invalid-feedback">La dirección es obligatoria para gestionar recogidas.</div>
-                </div>
+                <CustomInput
+                    id="direccion"
+                    label="Dirección"
+                    type="text"
+                    placeholder="Dirección (Calle, número, nave...)"
+                    required={true}
+                    hideLabel={true}
+                    errorMessage="La dirección es obligatoria para gestionar recogidas."
+                />
             )}
 
-            <div className="row g-3 mb-4">
+            <div className="row g-3">
                 <div className="col-md-4">
-                    <label htmlFor="cp" className="form-label d-none">Código Postal</label>
-                    <input
-                        type="text"
-                        className="form-control rounded-0 px-0 shadow-none text-primary inputs"
+                    <CustomInput
                         id="cp"
+                        label="Código Postal"
+                        type="text"
                         placeholder="C. Postal"
-                        pattern="[0-9]{5}"
+                        required={true}
+                        hideLabel={true}
+                        errorMessage="CP inválido."
+                        pattern="[0-9]{5}" // ¡Aquí funciona el ...props!
                         title="Debe contener 5 números"
-                        required
                     />
-                    <div className="invalid-feedback">CP inválido.</div>
                 </div>
                 <div className="col-md-8">
-                    <label htmlFor="provincia" className="form-label d-none">Provincia</label>
-                    <input
-                        type="text"
-                        className="form-control rounded-0 px-0 shadow-none text-primary inputs"
+                    <CustomInput
                         id="provincia"
+                        label="Provincia"
+                        type="text"
                         placeholder="Provincia / Ciudad"
-                        required
+                        required={true}
+                        hideLabel={true}
+                        errorMessage="Indica tu provincia."
                     />
-                    <div className="invalid-feedback">Indica tu provincia.</div>
                 </div>
             </div>
         </>
     );
 }
+
+
+// export default function DatosUbicacion({ tipoPerfil }) {
+//     return (
+//         <>
+//             {tipoPerfil === 'empresa' && (
+//                 <div className="mb-4">
+//                     <label htmlFor="direccion" className="form-label d-none">Dirección</label>
+//                     <input
+//                         type="text"
+//                         className="form-control rounded-0 px-0 shadow-none text-primary inputs"
+//                         id="direccion"
+//                         placeholder="Dirección (Calle, número, nave...)"
+//                         required
+//                     />
+//                     <div className="invalid-feedback">La dirección es obligatoria para gestionar recogidas.</div>
+//                 </div>
+//             )}
+
+//             <div className="row g-3 mb-4">
+//                 <div className="col-md-4">
+//                     <label htmlFor="cp" className="form-label d-none">Código Postal</label>
+//                     <input
+//                         type="text"
+//                         className="form-control rounded-0 px-0 shadow-none text-primary inputs"
+//                         id="cp"
+//                         placeholder="C. Postal"
+//                         pattern="[0-9]{5}"
+//                         title="Debe contener 5 números"
+//                         required
+//                     />
+//                     <div className="invalid-feedback">CP inválido.</div>
+//                 </div>
+//                 <div className="col-md-8">
+//                     <label htmlFor="provincia" className="form-label d-none">Provincia</label>
+//                     <input
+//                         type="text"
+//                         className="form-control rounded-0 px-0 shadow-none text-primary inputs"
+//                         id="provincia"
+//                         placeholder="Provincia / Ciudad"
+//                         required
+//                     />
+//                     <div className="invalid-feedback">Indica tu provincia.</div>
+//                 </div>
+//             </div>
+//         </>
+//     );
+// }
