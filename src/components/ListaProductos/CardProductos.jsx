38eb5a629276d4304ed28f2d5c9403ea8eb37bea
Ricardo Aucapiña <rl.aucapina@iesdoctorbalmis.com>
2026-03-06 20:17:12 +0100

    Se modificó el componente CardProducto para agregar el id={1} para cuando tengamos la base de datos y se linkeo el boton de Solicitar con la pagina Solicitud

--- src/components/ListaProductos/CardProductos.jsx
+++ src/components/ListaProductos/CardProductos.jsx
@@ -3,6 +3,7 @@
 import { Link } from "react-router-dom";
 
 export const CardProducto = ({
+  id,
   imagen,
   titulo,
   descripcion
@@ -10,14 +11,14 @@ export const CardProducto = ({
 }) => {
   return (
     <div className="col">
-      <div className='card shadow-lg'>
+      <div className='card shadow-lg h-100'>
         <img className='card-img-top' src={imagen} alt={titulo} />
         <div className='card-body d-flex flex-column'>
           <h5 className='card-title fw-bold text-dark'>{titulo}</h5>
           <p className='card-text text-dark'>{descripcion}</p>
           <div className="d-flex flex-row align-items-center justify-content-between">
             <div className="p-2 rounded-2 bg-success">Buen estado</div>
-            <Link to="/productos" className='btn btn-outline-success'>Solicitar</Link>
+            <Link to={`/solicitud/${id}`} className='btn btn-outline-success'>Solicitar</Link>
           </div>
         </div>
       </div>
--- src/components/ListaProductos/Lista.jsx
+++ src/components/ListaProductos/Lista.jsx
@@ -5,22 +5,27 @@ export default function Lista() {
         <div className="container">
             <div className="row row-cols-1 row-cols-lg-3 g-5">
                 <CardProducto
+                    id={1} // id={producto.id}
+                    imagen="https://picsum.photos/id/231/600/600" //imagen={producto.imagen}
+                    titulo="Portátiles" //titulo={producto.titulo} y descripcion={producto.descripcion}
+                    descripcion="Equipos reacondicionados listos para usar"></CardProducto>
+                <CardProducto
+                    id={1}
                     imagen="https://picsum.photos/id/231/600/600"
                     titulo="Portátiles"
                     descripcion="Equipos reacondicionados listos para usar"></CardProducto>
                 <CardProducto
+                    id={1}
                     imagen="https://picsum.photos/id/231/600/600"
                     titulo="Portátiles"
                     descripcion="Equipos reacondicionados listos para usar"></CardProducto>
                 <CardProducto
+                    id={1}
                     imagen="https://picsum.photos/id/231/600/600"
                     titulo="Portátiles"
                     descripcion="Equipos reacondicionados listos para usar"></CardProducto>
                 <CardProducto
-                    imagen="https://picsum.photos/id/231/600/600"
-                    titulo="Portátiles"
-                    descripcion="Equipos reacondicionados listos para usar"></CardProducto>
-                <CardProducto
+                    id={1}
                     imagen="https://picsum.photos/id/231/600/600"
                     titulo="Portátiles"
                     descripcion="Equipos reacondicionados listos para usar"></CardProducto>
