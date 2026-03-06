defe48c605109ce6487e0bfd31a087b1996a4a23
RicardoIESDB <rl.aucapina@iesdoctorbalmis.com>
2026-03-06 12:02:11 +0100

    Se agrega un desplegable en el CustomInput

--- src/components/CustomInput.jsx
+++ src/components/CustomInput.jsx
@@ -1,4 +1,4 @@
-const CustomInput = ({ id, label, type, placeholder, required, errorMessage, hideLabel, value, onChange, ...props}) => {
+const CustomInput = ({ id, label, type, placeholder, required, errorMessage, hideLabel, value, onChange, ...props }) => {
   // Si el input es un checkbox 
   if (type === "checkbox") {
     return (
@@ -40,6 +40,36 @@ const CustomInput = ({ id, label, type, placeholder, required, errorMessage, hid
         <div className="invalid-feedback">
           {errorMessage}
         </div>
+      </div>
+    );
+  }
+
+  if (type === "select") {
+    return (
+      <div className="w-100 mb-4">
+        <label htmlFor={id} className={`form-label fw-bold text-secondary ${hideLabel ? 'd-none' : ''}`}>
+          {label}
+        </label>
+        <select
+          className="form-select rounded-0 px-0 shadow-none text-primary inputs"
+          id={id}
+          name={id}
+          required={required}
+          value={value}
+          onChange={onChange}
+          {...props}
+        >
+          {/* Opción por defecto deshabilitada que actúa como "Placeholder" */}
+          <option value="" disabled>{placeholder}</option>
+
+          {/* Recorremos la lista de opciones que le pasemos */}
+          {options.map((opt, index) => (
+            <option key={index} value={opt.value}>
+              {opt.label}
+            </option>
+          ))}
+        </select>
+        <div className="invalid-feedback">{errorMessage}</div>
       </div>
     );
   }
